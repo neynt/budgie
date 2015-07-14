752360fe526fd72caa9bcfbbf49b15fd0f40a9d3
Jim Zhang <hyriodula@gmail.com>
2015-07-13 23:41:23 -0400

    add disc msg

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ io.on('connection', function(socket) {
   console.log('a user connected');
   socket.on('disconnect', function() {
     console.log('user disconnected');
+    io.emit('a user has left', msg);
   });
   socket.on('chat message', function(msg) {
     console.log('message: ' + msg);
@@ -17,6 +18,6 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(3000, function() {
-  console.log('listening on *:3000');
+http.listen(3069, function() {
+  console.log('listening on *:3069');
 });
