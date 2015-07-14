7004ddbbc04bfbf2f3270ef642d69730a54cf64b
Jim Zhang <hyriodula@gmail.com>
2015-07-13 23:43:57 -0400

    fix disc msg

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ io.on('connection', function(socket) {
   console.log('a user connected');
   socket.on('disconnect', function() {
     console.log('user disconnected');
-    io.emit('a user has left', msg);
+    io.emit('a user has left');
   });
   socket.on('chat message', function(msg) {
     console.log('message: ' + msg);
